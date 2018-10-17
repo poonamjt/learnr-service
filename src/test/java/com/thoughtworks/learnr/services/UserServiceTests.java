0e17c0afbe75dab8a78de1e3c11ae2e7f0768d36
Poonam <poonam@in-poonamjt.local>
2018-10-17 11:40:33 +0530

    +TB | Add testCase for UserService

--- src/test/java/com/thoughtworks/learnr/services/UserServiceTests.java
+++ src/test/java/com/thoughtworks/learnr/services/UserServiceTests.java
@@ -1,8 +1,11 @@
 package com.thoughtworks.learnr.services;
 
+import com.thoughtworks.learnr.models.User;
 import com.thoughtworks.learnr.repositories.UserRepository;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.mockito.Mockito.*;
@@ -19,8 +22,22 @@ public class UserServiceTests {
     }
 
     @Test
-    public void shouldCallRepository () {
+    public void shouldCallRepositoryFindAllMethod() {
         userService.getAllItems();
         verify( userRepository, times(1 ) ).findAll();
+    }
+
+    @Test
+    public void shouldCallRepositorySaveMethod(){
+        String name = "testUser";
+        long userId = 12l;
+        userService.addUser(userId, name);
+        ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
+        verify(userRepository, times(1)).save( userArgumentCaptor.capture() );
+
+        User actualUser = userArgumentCaptor.getValue();
+
+        Assert.assertEquals( name, actualUser.getName());
+        Assert.assertEquals( userId, actualUser.getUserId());
     }
 }
